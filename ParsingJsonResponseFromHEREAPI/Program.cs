8fb24b60a3800f4594525512e979a8a67cd6cc50
ArunKochukadavil <arunkochukadavil10@gmail.com>
2019-02-09 12:18:21 +0530

    commit

--- ParsingJsonResponseFromHEREAPI/Program.cs
+++ ParsingJsonResponseFromHEREAPI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -12,16 +13,27 @@ namespace ParsingJsonResponseFromHEREAPI
 	{
 		static void Main(string[] args)
 		{
-			WebClient wc = new WebClient();
-			Console.WriteLine("How many copies do you want to create : ");
-			int n = int.Parse(Console.ReadLine());
-			var add = AddFetcher.getAddress(n);
-			var app_id = "nlETxVb8XzBCAp720JMZ";
-			var app_code = "MqWTqxG7PNX4VbNrmJ_OpA";
-			var latLong = Fetcher.GetLatLong(app_id, app_code, add);
-			foreach(var val in latLong)
+			try
 			{
-				Console.WriteLine($"Latitude = {val.Latitude} & Longitude = {val.Longitude}");
+				WebClient wc = new WebClient();
+				Console.WriteLine("How many copies do you want to create : ");
+				int n = int.Parse(Console.ReadLine());
+				var text = File.ReadAllText(@"C:\Users\Arun\Documents\Visual Studio 2017\HereAPIKeys.txt");
+				var id = text.Split();
+
+				var add = AddFetcher.getAddress(n);
+				var app_id = id[0];
+				var app_code = id[1];
+
+				var latLong = Fetcher.GetLatLong(app_id, app_code, add);
+				foreach (var val in latLong)
+				{
+					Console.WriteLine($"Latitude = {val.Latitude} & Longitude = {val.Longitude}");
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
 			}
 		}
 	}
